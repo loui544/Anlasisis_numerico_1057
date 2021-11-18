8e66ecfd86fc1f9e93bab62b4e6f8077ae6713e7
loui544 <loui_vel@hotmail.com>
2021-11-18 17:56:47 -0500

    Update final.py

--- Trabajo_final/final.py
+++ Trabajo_final/final.py
@@ -63,12 +63,14 @@ def plot_with_death_rate(S, I, R, t, divide_by=1, death_rate=0.05):
     ax.set_ylabel(f'Número (dividido por {divide_by:,})')
     legend = ax.legend()
     print("")
-    fig.show()
+    # fig.show()
     
 # población inicial, N.
 N = 479000 # poblaciçon de un país como España
  
 # Número inicial de infectados y recuperados, I0 and R0.
+# I0 = 14/N
+# R0 = 7/N
 I0 = 2/N
 R0 = 0
 C0 = 1.5
@@ -90,14 +92,14 @@ y0 = S0, I0, R0
 ret = odeint(deriv, y0, t, args=(N, beta, gamma))
 S, I, R = ret.T
 
-# print(I)
+# print("Susceptibles: " + str(S))
+
+# print("Infectados: " + str(I))
+
+# print("Recuperados: " + str(R))
 
 # taylor(deriv,a,b,1,365,3)
  
 # plot(S, I, R, t) # Datos sin normalizar
-#plot(S, I, R, t, divide_by=N) # Datos normalizados
-# plot_with_death_rate(S, I, R, t, divide_by=N, death_rate=0.05)
-print("R_e(t):\n")
-for t in range(61):
-    print((beta*C0*S[t])/(gamma*N))
-    
+# plot(S, I, R, t, divide_by=N) # Datos normalizados
+plot_with_death_rate(S, I, R, t, divide_by=N, death_rate=0.05)
